ce9bcf3054836e209f6213d46fe974ce37f54129
marcusalves13 <marcusv.alves076@gmail.com>
2022-06-09 18:49:20 -0300

    Prova Desenvolvimento com Framework

---
+++ src/marcusva/MedicoDAO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marcusva;
+
+/**
+ *
+ * @author Marquinho
+ */
+public class MedicoDAO extends Conexao {
+       public String cadastrarMedico(Medico medico){
+           try
+           {
+               String sentenca;
+               sentenca = "INSERT INTO medico VALUES (NULL, '" 
+                    + medico.getNome() 
+                    + "','" + medico.getEspecialidade() 
+                    + "','" + medico.getCrm() + "','" + medico.getCpf() + "')";
+               return this.atualizarBanco(sentenca);
+           }
+           
+            catch(Exception e)
+            {
+                return e.getMessage();
+            }
+       }
+}
---
+++ src/marcusva/Paciente.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package marcusva;
+
+/**
+ *
+ * @author Marquinho
+ */
+public class Paciente {
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    private String nome;
+    private int idade;
+    private String cpf;
+}
